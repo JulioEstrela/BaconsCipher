3f56b93495cf87229261422c4d4bfe7e4fb4f5b7
Julio <97416841+JulioEstrela@users.noreply.github.com>
2022-08-26 23:59:07 -0300

    Main file

---
+++ FrancisBacon.py
@@ -0,0 +1,35 @@
+import textwrap as tw
+import string
+
+alphabet = list(string.ascii_lowercase)
+
+baconCode = ["AAAAA", "AAAAB", "AAABA", "AAABB", "AABAA", "AABAB", "AABBA",
+"AABBB", "ABAAA", "ABAAB", "ABABA", "ABABB", "ABBAA", "ABBAB", "ABBBA",
+"ABBBB", "BAAAA", "BAAAB", "BAABA", "BAABB", "BABAA", "BABAB", "BABBA", 
+"BABBB","BBAAA", "BBAAB"]
+
+text = input()
+temp = ""
+res = []
+
+for i in text:
+    if i >= 'a' and i <= 'z':
+        i = 'A'
+    elif i >= 'A' and i <='Z':
+        i = 'B'
+    else:
+        i = ''
+    temp += i
+
+#separar em uma lista de 5 elementos cada chunk
+temp = tw.wrap(temp, 5)
+
+for chunk in temp:
+    for item in baconCode:
+        if item == chunk:
+            item = alphabet[baconCode.index(item)]
+            res += item
+
+res = "".join(res)
+print(temp)
+print(res)
